39a2a6c3a9787fa330b4c0b3072ded79799066e7
FreeMasen <r.f.masen@gmail.com>
2015-03-31 14:42:15 -0500

    function yelp bot, working on second window

---
+++ PatIO/MainWindow.xaml.vb
@@ -0,0 +1,57 @@
+﻿Class MainWindow 
+    Dim yelpinfo As New Yelpbot
+    Dim listing As New Listing
+
+    Private Sub MainWindow_Loaded(sender As Object, e As RoutedEventArgs) Handles Me.Loaded
+        setDropDowns()
+    End Sub
+
+    Private Sub setDropDowns()
+        Dim dates As New ArrayList
+
+        For item As Integer = 0 To 6
+            Dim datetoadd As Date = Now.AddDays(item)
+            If datetoadd = Now Then
+                dates.Add("Today")
+            ElseIf datetoadd = Now.AddDays(1) Then
+                dates.Add("Tomorrow")
+            ElseIf datetoadd > Now.AddDays(1) Then
+                dates.Add(datetoadd.DayOfWeek.ToString)
+            End If
+        Next
+        For Each item In dates
+            cboDate.Items.Add(item.ToString)
+        Next
+        Dim states As String() = {"AL", "AK", "AZ", "Arkansas", "CA", "CO", "CT", "Delaware", _
+                                  "Florida", "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", _
+                                  "Louisiana", "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota", "Mississippi", "Missouri", _
+                                  "Montana", "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York", "North Carolina", _
+                                  "North Dakota", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island", "South Carolina", _
+                                  "South Dakota", "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington", "West Virginia", _
+                                  "Wisconsin", "Wyoming"}
+
+        For Each item In states
+            cboState.Items.Add(item)
+        Next
+        Dim times As Array = {"Lunch", "Dinner", "Drinks"}
+        For Each item In times
+            cboTime.Items.Add(item)
+        Next
+    End Sub
+
+
+    Private Sub btnSubmit_Click(sender As Object, e As RoutedEventArgs) Handles btnSubmit.Click
+
+        yelpinfo.buildURL(txtCity.Text, "")
+        listing.lblBiz1Address.Content = yelpinfo.names(0).ToString
+        listing.lblBiz1Address.Content = yelpinfo.locations(0).ToString
+        listing.lblBiz1Rating.Content = yelpinfo.ratings(0).ToString
+        listing.lblBiz2Name.Content = yelpinfo.names(1).ToString
+        listing.lblBiz2Address.Content = yelpinfo.locations(1).ToString
+        listing.lblBiz1Rating.Content = yelpinfo.ratings(1).ToString
+        listing.lblBiz3Name.Content = yelpinfo.names(2).ToString
+        listing.lblBiz3Address.Content = yelpinfo.locations(2).ToString
+        listing.lblBiz3Rating.Content = yelpinfo.ratings(2).ToString
+        listing.Show()
+    End Sub
+End Class
---
+++ PatIO/YelpBot.vb
@@ -0,0 +1,72 @@
+﻿Imports System.Net
+Imports System
+Imports System.IO
+Imports System.Text
+Imports SimpleOAuth
+Imports System.Xml
+
+Public Class Yelpbot
+    Public names As XmlNodeList
+    Public locations As XmlNodeList
+    Public ratings As XmlNodeList
+    Dim xml As XmlDocument = New XmlDocument
+    Public zip As String
+
+
+
+    Public Sub getXML(ByVal searchURL As String)
+        Dim oauth_consumer_key As String = "aq4s508pN0XAgmgvw0shFQ"
+        Dim consumer_secret As String = "0iujoBA_f2bpTAZuwHobPX90TyI"
+        Dim oauth_token As String = "v07rB7TFgu8iRmvIXneieNCdFwTxH-w3"
+        Dim oauth_token_secret As String = "rbAlZxdsim6m90YpnvLfR6AQ6Mk"
+
+        Dim OTokens As Tokens = New Tokens()
+        OTokens.ConsumerKey = oauth_consumer_key
+        OTokens.ConsumerSecret = consumer_secret
+        OTokens.AccessToken = oauth_token
+        OTokens.AccessTokenSecret = oauth_token_secret
+
+        Dim request As HttpWebRequest = CType(WebRequest.Create(searchURL), HttpWebRequest)
+        request.SignRequest(OTokens).WithEncryption(EncryptionMethod.HMACSHA1).InHeader()
+
+        Dim response As HttpWebResponse = CType(request.GetResponse, HttpWebResponse)
+        Dim reader As Stream = response.GetResponseStream
+
+        xml.Load(reader)
+
+        reader.Close()
+
+        xml.Save("yelpexample")
+    End Sub
+
+    Public Function buildURL(ByVal city As String, Optional ByVal state As String = "") As String
+
+        Dim searchLocation As String
+        If state.Length > 0 Then
+            searchLocation = String.Format("{0}+{1}", city, state)
+        ElseIf city.Contains(" ") Then
+            city.Replace(" ", "+")
+        Else
+            searchLocation = city
+        End If
+
+        Return String.Format("http://api.yelp.com/v2/search?term=patio&location={0}&category_filter=restaurants&sort=2&radius_filter=1700&limit=5&output=xml", searchLocation)
+
+    End Function
+
+    Public Sub setNodeLists()
+
+        names = xml.SelectNodes("//name")
+        locations = xml.SelectNodes("//address")
+        ratings = xml.SelectNodes("//rating")
+
+    End Sub
+
+    Public Function returnZip() As String
+        Dim zipcode As XmlNode = xml.SelectSingleNode("//postal_code")
+        zip = zipcode.InnerText.ToString
+        Return zip
+    End Function
+
+End Class
+
